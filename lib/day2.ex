6b85948a1e6cba26d8a82913c275582d80af9f37
Francisco Krause Arnim <fkrausear@gmail.com>
2022-12-03 01:00:51 -0300

    Solve day 2 part 2

--- lib/day2.ex
+++ lib/day2.ex
@@ -5,10 +5,21 @@ defmodule Advent2022.Day2 do
     Advent2022.Input.get(:day2, :test)
     |> advent_game()
   end
+
   def solve_input do
     Advent2022.Input.get(:day2)
     |> advent_game()
   end
 
+  def solve_test_input_part_2 do
+    Advent2022.Input.get(:day2, :test)
+    |> advent_game_part_2()
+  end
+
+  def solve_input_part_2 do
+    Advent2022.Input.get(:day2)
+    |> advent_game_part_2()
+  end
   def advent_game(input) when is_list(input), do: :erlang.nif_error(:nif_not_loaded)
+  def advent_game_part_2(input) when is_list(input), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/advent_rust/src/lib.rs
+++ native/advent_rust/src/lib.rs
@@ -28,6 +28,22 @@ fn game_to_score(elf_game: ElfGame, my_game: MyGame) -> u64 {
         (ElfGame::C, MyGame::Z) => 3 + 3,
     }
 }
+#[inline]
+fn game_to_score_part_2(elf_game: ElfGame, my_game: MyGame) -> u64 {
+    let new_mine =
+        match (&elf_game, my_game) {
+        (ElfGame::A, MyGame::X) => MyGame::Z,
+        (ElfGame::A, MyGame::Y) => MyGame::X,
+        (ElfGame::A, MyGame::Z) => MyGame::Y,
+        (ElfGame::B, MyGame::X) => MyGame::X,
+        (ElfGame::B, MyGame::Y) => MyGame::Y,
+        (ElfGame::B, MyGame::Z) => MyGame::Z,
+        (ElfGame::C, MyGame::X) => MyGame::Y,
+        (ElfGame::C, MyGame::Y) => MyGame::Z,
+        (ElfGame::C, MyGame::Z) => MyGame::X,
+    };
+    return game_to_score(elf_game, new_mine);
+}
 #[rustler::nif]
 fn advent_game(input: Vec<String>) -> u64 {
     let parsed_input: Vec<Vec<&String>> = input
@@ -50,4 +66,26 @@ fn advent_game(input: Vec<String>) -> u64 {
     return solution;
 }
 
-rustler::init!("Elixir.Advent2022.Day2", [advent_game]);
+#[rustler::nif]
+fn advent_game_part_2(input: Vec<String>) -> u64 {
+    let parsed_input: Vec<Vec<&String>> = input
+        .iter()
+        .chunks(2)
+        .into_iter()
+        .map(|chunk| chunk.collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let solution: u64 =
+        parsed_input
+        .into_iter()
+        .map(|game| -> u64 {
+            let score = game_to_score_part_2(
+                ElfGame::from_str(game[0]).unwrap(),
+                MyGame::from_str(game[1]).unwrap(),
+            );
+            return score;
+        })
+        .sum();
+    return solution;
+}
+
+rustler::init!("Elixir.Advent2022.Day2", [advent_game, advent_game_part_2]);
