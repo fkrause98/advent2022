7d0d5c772c5cd2f7d2a8ac1cd4c86f31a5c70766
Francisco Krause Arnim <fkrausear@gmail.com>
2022-12-06 23:29:09 -0300

    Rename funs

--- lib/day6.ex
+++ lib/day6.ex
@@ -8,7 +8,7 @@ defmodule Advent2022.Day6 do
     def solve_test_input_part_1(test) do
       test
       |> String.to_charlist()
-      |> generate_possible_markers()
+      |> possible_markers()
       |> Enum.find_index(&is_marker/1)
       |> Kernel.+(4)
     end
@@ -16,22 +16,22 @@ defmodule Advent2022.Day6 do
     def solve_part_1 do
       Advent2022.Input.get(:day6)
       |> String.to_charlist()
-      |> generate_possible_markers()
+      |> possible_markers()
       |> Enum.find_index(&is_marker/1)
       |> Kernel.+(4)
     end
 
     def is_marker(list), do: Enum.uniq(list) == list
 
-    def generate_possible_markers(str) when is_list(str) do
-      generate_possible_markers(str, [])
+    def possible_markers(str) when is_list(str) do
+      possible_markers(str, [])
     end
 
-    defp generate_possible_markers(str = [a, b, c, d | _], acum) do
-      generate_possible_markers(tl(str), [[a, b, c, d] | acum])
+    defp possible_markers(str = [a, b, c, d | _], acum) do
+      possible_markers(tl(str), [[a, b, c, d] | acum])
     end
 
-    defp generate_possible_markers(_, acum), do: Enum.reverse(acum)
+    defp possible_markers(_, acum), do: Enum.reverse(acum)
   end
 
   defmodule Part2 do
@@ -43,7 +43,7 @@ defmodule Advent2022.Day6 do
     def solve_test_input(test) do
       test
       |> String.to_charlist()
-      |> generate_possible_markers()
+      |> possible_markers()
       |> Enum.find_index(&is_marker/1)
       |> Kernel.+(14)
     end
@@ -51,22 +51,22 @@ defmodule Advent2022.Day6 do
     def solve do
       Advent2022.Input.get(:day6)
       |> String.to_charlist()
-      |> generate_possible_markers()
+      |> possible_markers()
       |> Enum.find_index(&is_marker/1)
       |> Kernel.+(14)
     end
 
     def is_marker(list), do: Enum.uniq(list) == list
 
-    def generate_possible_markers(str) when is_list(str) do
-      generate_possible_markers(str, [])
+    def possible_markers(str) when is_list(str) do
+      possible_markers(str, [])
     end
 
-    defp generate_possible_markers(str = [_ | tail], acum) when length(str) >= 14 do
+    defp possible_markers(str = [_ | tail], acum) when length(str) >= 14 do
       possible_marker = Enum.take(str, 14)
-      generate_possible_markers(tail, [possible_marker | acum])
+      possible_markers(tail, [possible_marker | acum])
     end
 
-    defp generate_possible_markers(_, acum), do: Enum.reverse(acum)
+    defp possible_markers(_, acum), do: Enum.reverse(acum)
   end
 end
